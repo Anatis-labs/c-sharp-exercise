6ae99188a7195d4d5418c521aae4b068e721bef7
Daniel Broberg <broberg.daniel89@gmail.com>
2019-11-20 10:27:10 +0100

    update

---
+++ main-exercises/Exercise-18/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int[] number1 = new int[10];
+            double[] number2 = new double[10];
+
+            for (int i = 0; i <= 9; i++)
+            {
+                number1[i] = random.Next(1, 100);
+            }
+
+            foreach (int print1 in number1)
+            {
+                Console.WriteLine(print1);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i <= 9; i++)
+            {
+                number2[i] = (1/Convert.ToDouble(number1[i]));
+            }
+
+            foreach(double print2 in number2)
+            {
+                Console.WriteLine(print2);
+            }
+        }
+    }
+}
+
+
+
+
---
+++ main-exercises/exercise-17/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace exercise_17
+{
+    class Program
+    {
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to see if its a palindrome sentence, simply put if u can flip it and its still the same word.");
+            Console.WriteLine("The program will end when u find one");
+            while (true)
+            {
+                String input = Console.ReadLine();
+                String Compare = ReverseString(input);
+
+                if (input == Compare)
+                {
+                    Console.WriteLine("hurray you found one, the program has done its task, and is going to rest.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry not one, try again");
+                }
+                Console.WriteLine("the word u wrote:");
+                Console.WriteLine(input);
+                Console.WriteLine("The word u wrote in reverse");
+                Console.WriteLine(Compare);
+            }
+        }
+    }
+}
