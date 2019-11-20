c9d600f8a5a839b8cc49365a565cb316377ae2f7
Daniel Broberg <broberg.daniel89@gmail.com>
2019-11-20 12:29:04 +0100

    update

--- exercise-08/exercise-08/Program.cs
+++ exercise-08/exercise-08/Program.cs
@@ -6,7 +6,22 @@ namespace exercise_08
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Let the user input a decimal number. Then output square root of the
+            number and the number raised to the power of 2 and 10.That is √n , n2
+            (square root 2), n10(square root 10).
+            */
+
+            Console.WriteLine("Please enter a number to do magic math with.");
+            decimal input = Convert.ToDecimal(Console.ReadLine());
+            
+            //raised
+            Console.WriteLine("The number you entered raised by 2 is: "+Math.Pow(Convert.ToDouble(input), 2));
+            Console.WriteLine("The number you entered raised by 10 is: " + Math.Pow(Convert.ToDouble(input), 10));
+
+            //squared
+            Console.WriteLine("The squareroot of the number you entered is: " +Math.Sqrt(Convert.ToDouble(input)));
+            
         }
     }
 }
---
+++ main-exercises/Exercise-14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+            int sum = 0;
+            Console.WriteLine("Enter as many numbers as u want, \n if u enter -1 the program will give u the sumaverage of the entered numbers");
+            while (true)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    intList.Add(number);
+                }
+            }
+
+            foreach (var print in intList)
+            {
+                sum = (sum + print);
+            }
+            Console.WriteLine("The sum of the numbers is:" + sum);
+            if (intList.Count > 0)
+            {
+                double average = intList.Average();
+                Console.WriteLine("The average of the numbers are:" + average);
+            }
+        }
+    }
+}
